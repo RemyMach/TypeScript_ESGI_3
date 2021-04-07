7826a9088441c3822cadb19e78893b3d6a74897a
RemyMach <remygt@hotmail.fr>
2021-04-07 11:29:14 +0200

    move fxtures for the tests in a separate folder

---
+++ tests/fixtures/pokemon.ts
@@ -0,0 +1,96 @@
+import {Pokemon} from '../../pokemon';
+export const carapuce = new Pokemon({
+    name: "carapuce",
+    health: 80,
+    speed: 80,
+    attack: [
+        {
+            name: "charge",
+            value: 10
+        },
+        {
+            name: "pisotlet à eau",
+            value: 30
+        },
+        {
+            name: "ecume",
+            value: 35
+        },
+        {
+            name: "trempette",
+            value: 5
+        },
+    ]}
+);
+
+export const pikachu = new Pokemon({
+    name: "pikachu",
+    health: 80,
+    speed: 70,
+    attack: [
+        {
+            name: "charge",
+            value: 10
+        },
+        {
+            name: "eclair",
+            value: 30
+        },
+        {
+            name: "fatal Tonerre",
+            value: 35
+        },
+        {
+            name: "griffe",
+            value: 5
+        },
+    ]}
+);
+
+export const papilusion = new Pokemon({
+    name: "papilusion",
+    health: 90,
+    speed: 40,
+    attack: [
+        {
+            name: "vol",
+            value: 10
+        },
+        {
+            name: "poudre dodo",
+            value: 30
+        },
+        {
+            name: "cru-aile",
+            value: 35
+        },
+        {
+            name: "charge",
+            value: 5
+        },
+    ]}
+);
+
+export const other_pikachu = new Pokemon({
+    name: "pikachu 2",
+    health: 80,
+    speed: 70,
+    attack: [
+        {
+            name: "charge",
+            value: 10
+        },
+        {
+            name: "eclair",
+            value: 30
+        },
+        {
+            name: "fatal Tonerre",
+            value: 35
+        },
+        {
+            name: "griffe",
+            value: 5
+        },
+    ]
+});
--- tests/pokemon.test.ts
+++ tests/pokemon.test.ts
@@ -1,78 +1,7 @@
 import {Pokemon} from '../pokemon';
+import {carapuce, pikachu, papilusion, other_pikachu} from './fixtures/pokemon';
+
 describe('test Jest', () => {
-
-
-    let carapuce = new Pokemon({
-        name: "carapuce",
-        health: 80,
-        speed: 80,
-        attack: [
-            {
-                name: "charge",
-                value: 10
-            },
-            {
-                name: "pisotlet à eau",
-                value: 30
-            },
-            {
-                name: "ecume",
-                value: 35
-            },
-            {
-                name: "trempette",
-                value: 5
-            },
-        ]}
-    );
-
-    let pikachu = new Pokemon({
-        name: "pikachu",
-        health: 80,
-        speed: 70,
-        attack: [
-            {
-                name: "charge",
-                value: 10
-            },
-            {
-                name: "eclair",
-                value: 30
-            },
-            {
-                name: "fatal Tonerre",
-                value: 35
-            },
-            {
-                name: "griffe",
-                value: 5
-            },
-        ]}
-    );
-
-    let papilusion = new Pokemon({
-        name: "papilusion",
-        health: 90,
-        speed: 40,
-        attack: [
-            {
-                name: "vol",
-                value: 10
-            },
-            {
-                name: "poudre dodo",
-                value: 30
-            },
-            {
-                name: "cru-aile",
-                value: 35
-            },
-            {
-                name: "charge",
-                value: 5
-            },
-        ]}
-    );
 
     const determineWhoAttackInFirst = (pok1: Pokemon, pok2: Pokemon): Pokemon | null => {
         if(pok1.speed === pok2.speed) {
@@ -95,29 +24,6 @@ describe('test Jest', () => {
 
     it('Should return null when the 2 pokemon have the same speed', () => {
         
-        let other_pikachu = new Pokemon({
-            name: "pikachu 2",
-            health: 80,
-            speed: 70,
-            attack: [
-                {
-                    name: "charge",
-                    value: 10
-                },
-                {
-                    name: "eclair",
-                    value: 30
-                },
-                {
-                    name: "fatal Tonerre",
-                    value: 35
-                },
-                {
-                    name: "griffe",
-                    value: 5
-                },
-            ]
-        });
         expect(determineWhoAttackInFirst(pikachu, other_pikachu)).toBe(other_pikachu);
     })
 });
